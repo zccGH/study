659d7cc0bb61bc4dda47a006c24ef98adeefe631
zcc <zcc1330214747@163.com>
2020-08-20 23:08:41 +0800

    spring mvc模块添加测试接口

---
+++ springmvc/src/main/java/com/zcc/study/springmvc/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zcc.study.springmvc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 赵成成
+ * @version 1.0
+ * @Description 测试接口
+ * @date
+ */
+@RestController
+public class TestController {
+}
